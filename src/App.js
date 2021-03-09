6ec153ae457f76e7d7914cee8ef7796174acf52e
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-03-09 18:42:44 +0700

    ADD:styled app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import "./App.css";
+import styled from "styled-components";
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> hello </h1>
-      </header>
-    </div>
+    <Wrapper>
+      <AppContainer>
+        <AppBody>ok hello</AppBody>
+      </AppContainer>
+    </Wrapper>
   );
-}
+};
 
 export default App;
+
+const Wrapper = styled.div`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const AppContainer = styled.div`
+  background-color: lightblue;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const AppBody = styled.div`
+  background-color: #ededed;
+  height: 90vh;
+  width: 90vw;
+  margin-top: -50px;
+  box-shadow: -1px 4px 20px -6px rgba(0, 0, 0, 0.2);
+`;
